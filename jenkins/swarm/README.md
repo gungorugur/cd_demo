8ef21479bddf873fd3f20e811e7cfcc46769de68
gungorugur <gngrugur@gmail.com>
2018-12-17 21:23:57 +0300

    #Added jenkins swarm client.

---
+++ jenkins/swarm/README.md
@@ -0,0 +1,8 @@
+# Docker Swarm Client
+
+Run given command  to connecting to jenkins master as a docker in docker slave.
+
+```bash
+./run.sh http://jenkins_url:port username password example-label
+```
+I use this container in swarm master node to deploying applications across to worker nodes.
---
+++ jenkins/swarm/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+JENKINS_URL=$1
+JENKINS_USER=$2
+JENKINS_PASS=$3
+JENKINS_LABELS=$3
+
+sudo chmod 777 /var/run/docker.sock
+
+echo Connecting to $JENKINS_URL
+
+java -jar client.jar -master $JENKINS_URL -username $JENKINS_USER -password $JENKINS_PASS -labels $JENKINS_LABELS -executors 2 -mode exclusive
