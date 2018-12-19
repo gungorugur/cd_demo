25d5fec73242127c487316874db5d342c6eb162b
gungorugur <gngrugur@gmail.com>
2018-12-19 20:54:48 +0300

    Configured swarm deployer always restart mode

--- jenkins/swarm-deployer/run.sh
+++ jenkins/swarm-deployer/run.sh
@@ -5,4 +5,4 @@ JENKINS_LABELS=$3
 
 docker build . -t ugurgungor/deployer:latest
 
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/bin/docker ugurgungor/deployer:latest $JENKINS_URL $JENKINS_USER $JENKINS_PASS $JENKINS_LABELS
+docker run --restart=always -d -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/bin/docker ugurgungor/deployer:latest $JENKINS_URL $JENKINS_USER $JENKINS_PASS $JENKINS_LABELS
